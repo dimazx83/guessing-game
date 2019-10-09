b8eba26b391e5a07cd874d17d7ba9c2af0a8cf2f
Марина Пискуненко <C:\Users\Asus\AppData\Roaming\The Bat!\Mail>
2019-10-09 17:55:29 +0300

    break algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.m = [];
+  }
 
-    setRange(min, max) {
-
+  setRange(min, max) {
+    for (let i = min; i < max + 1; i++) {
+      this.m[i] = i;
     }
+  }
 
-    guess() {
+  guess() {
+    return this.m[0] + Math.round((this.m[this.m.length - 1] - this.m[0]) / 2);
+  }
 
-    }
+  lower() {
+    this.m = this.m.slice(0, this.m.indexOf(this.guess()));
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.m = this.m.slice(this.m.indexOf(this.guess() + 1));
+  }
 }
 
 module.exports = GuessingGame;
